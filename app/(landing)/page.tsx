6723643581af35522819d3969c80cc438ae0b50b
Tisca Catalin <admin@tiscacatalin.com>
2025-11-20 10:51:20 +0200

    finshed the landing page template

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,14 @@
 import HeroSection from "./components/hero";
 import FeaturesSection from "./components/features";
+import CTASection from "./components/cta";
 
 export default function LandingPage() {
-  return (
-    <>
-      <HeroSection />
-      <FeaturesSection />
-    </>
-  );
+    return (
+        <>
+            <HeroSection />
+            <FeaturesSection />
+            <CTASection />
+        </>
+    );
 }
+
