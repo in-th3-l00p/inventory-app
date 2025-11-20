2920307259097e8c966e44f92ace67a9aa5ac669
Tisca Catalin <admin@tiscacatalin.com>
2025-11-20 10:45:26 +0200

    added the footer

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,12 +1,16 @@
-import Header from "./components/header";
+import Header from "./components/layout/header";
+import Footer from "./components/layout/footer";
 
 export default function LandingLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="min-h-screen overflow-x-hidden">
-            <Header />
-            <main>
-                {children}
-            </main>
-        </div>
+        <>
+            <div className="min-h-screen overflow-x-hidden">
+                <Header />
+                <main>
+                    {children}
+                </main>
+            </div>
+            <Footer />
+        </>
     );
 }
