143429a0a9947e11ba33a99b40bccba0e1deb67d
Tisca Catalin <admin@tiscacatalin.com>
2025-11-22 18:54:09 +0200

    absolute header

--- app/(landing)/components/layout/header.tsx
+++ app/(landing)/components/layout/header.tsx
@@ -53,7 +53,7 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="relative isolate z-10 bg-white">
+    <header className="absolute w-screen isolate z-10 bg-white">
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
